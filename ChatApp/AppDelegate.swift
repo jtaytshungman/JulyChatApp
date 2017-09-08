048e62c6b984027bcb20f7fbd43a9012a68f4165
jeremyttm <j.taytshungman@gmail.com>
2017-09-08 14:15:43 +0800

    login as first page

--- ChatApp/AppDelegate.swift
+++ ChatApp/AppDelegate.swift
@@ -16,6 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let authStoryboard = UIStoryboard(name: "Auth", bundle: Bundle.main)
+        let loginVC = authStoryboard.instantiateViewController(withIdentifier: "AuthNavigation")
+        
+        window?.rootViewController = loginVC
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
